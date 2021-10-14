874317e22fe3c9c40481a0587f2b35b9137ac0d6
Adrian Pantea <adrianpantea10@gmail.com>
2021-10-14 09:47:29 +0100

    added search section

--- mindfuleducation/src/App.js
+++ mindfuleducation/src/App.js
@@ -1,9 +1,12 @@
 
 import Home from './Components/Home'
+import Search from './Components/Search'
 
 function App() {
   return (
     <div className="app">
+      <h2>Partners</h2>
+      <Search />
       <Home />
     </div>
   );
