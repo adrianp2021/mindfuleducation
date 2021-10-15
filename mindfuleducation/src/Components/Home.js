c4be2506666fe7abd45ccb34e70cd24fe687449f
Adrian Pantea <adrianpantea10@gmail.com>
2021-10-15 23:19:25 +0100

    return data from json in table

--- mindfuleducation/src/Components/Home.js
+++ mindfuleducation/src/Components/Home.js
@@ -1,40 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Table, Image } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+import axios from "axios";
 
-import logo from "../logo/mindful.png";
+const Home = () => {
+  const url =
+    "https://mindfuleducation-cdn.s3.eu-west-1.amazonaws.com/misc/data.json";
+  const [product, setProduct] = useState([]);
 
-const Home = () => (
-  <Table celled padded>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell singleLine>Partner</Table.HeaderCell>
-        <Table.HeaderCell>Prefix</Table.HeaderCell>
-        <Table.HeaderCell>Logo/Preroll</Table.HeaderCell>
-        <Table.HeaderCell>Ofsted Rating</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setProduct(response.data.getColleges);
+    });
+  }, []);
+  console.log(product);
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>
-          <Header as="h5" textAlign="left">
-            C Mindful College
-          </Header>
-        </Table.Cell>
-        <Table.Cell singleLine>CMC</Table.Cell>
-        <Table.Cell id="centre">
-          <Image src={logo} className="logo" />
-        </Table.Cell>
-
-        <Table.Cell textAlign="right">
-          <Header as="h5" textAlign="left">
-            Requires improvement
-          </Header>
-        </Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-);
+  return (
+    <Table celled padded>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell singleLine>Partner</Table.HeaderCell>
+          <Table.HeaderCell>Prefix</Table.HeaderCell>
+          <Table.HeaderCell>Logo/Preroll</Table.HeaderCell>
+          <Table.HeaderCell>Ofsted Rating</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      {product.map((item) => {
+        return (
+          <>
+            <Table.Body key={item}>
+              <Table.Row>
+                <Table.Cell>
+                  <Header as="h5" textAlign="left">
+                    {item.name}
+                  </Header>
+                </Table.Cell>
+                <Table.Cell singleLine>{item.groupPrefix}</Table.Cell>
+                <Table.Cell id="centre">
+                  <Image src={item.logo} className="logo" />
+                </Table.Cell>
+                <Table.Cell textAlign="right">
+                  <Header as="h5" textAlign="left">
+                    {item.ofstedRating}
+                  </Header>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </>
+        );
+      })}
+    </Table>
+  );
+};
 
 export default Home;
