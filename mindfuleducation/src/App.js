e857d68113b809ec8d8370a800414a39b216ac12
Adrian Pantea <adrianpantea10@gmail.com>
2021-10-18 14:26:50 +0100

    search - filter through the names

--- mindfuleducation/src/App.js
+++ mindfuleducation/src/App.js
@@ -1,12 +1,10 @@
 
 import Home from './Components/Home'
-import Search from './Components/Search'
 
 function App() {
   return (
     <div className="app">
       <h2>Partners</h2>
-      <Search />
       <Home />
     </div>
   );
--- mindfuleducation/src/Components/Home.js
+++ mindfuleducation/src/Components/Home.js
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Header, Table, Image } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
+import { Form } from "semantic-ui-react";
 
 const Home = () => {
   const url =
     "https://mindfuleducation-cdn.s3.eu-west-1.amazonaws.com/misc/data.json";
   const [product, setProduct] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const options = [
+    { key: "o", text: "Outstanding", value: "outstanding" },
+    { key: "g", text: "Good", value: "good" },
+    { key: "r", text: "Requires improvement", value: "requires improvement" },
+    { key: "i", text: "Inadequate", value: "inadequate" },
+    { key: "na", text: "NA", value: "na" },
+  ];
 
   useEffect(() => {
     axios.get(url).then((response) => {
@@ -16,40 +26,67 @@ const Home = () => {
   console.log(product);
 
   return (
-    <Table celled padded>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell singleLine>Partner</Table.HeaderCell>
-          <Table.HeaderCell>Prefix</Table.HeaderCell>
-          <Table.HeaderCell>Logo/Preroll</Table.HeaderCell>
-          <Table.HeaderCell>Ofsted Rating</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      {product.map((item) => {
-        return (
-          <>
-            <Table.Body key={item}>
-              <Table.Row>
-                <Table.Cell>
-                  <Header as="h5" textAlign="left">
-                    {item.name}
-                  </Header>
-                </Table.Cell>
-                <Table.Cell singleLine>{item.groupPrefix}</Table.Cell>
-                <Table.Cell id="centre">
-                  <Image src={item.logo} className="logo" />
-                </Table.Cell>
-                <Table.Cell textAlign="right">
-                  <Header as="h5" textAlign="left">
-                    {item.ofstedRating}
-                  </Header>
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </>
-        );
-      })}
-    </Table>
+    <>
+      <Form className="search">
+        <Form.Group widths="equal">
+          <Form.Input
+            fluid
+            label="Name"
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+          <Form.Input fluid label="Prefix" />
+          <Form.Select fluid label="Ofsted Rating" options={options} />
+        </Form.Group>
+        <Form.Button>Apply</Form.Button>
+      </Form>
+
+      <Table celled padded>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell singleLine>Partner</Table.HeaderCell>
+            <Table.HeaderCell>Prefix</Table.HeaderCell>
+            <Table.HeaderCell>Logo/Preroll</Table.HeaderCell>
+            <Table.HeaderCell>Ofsted Rating</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        {product
+          .filter((val) => {
+            if (searchTerm === "") {
+              return val;
+            } else if (
+              val.name.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((item) => {
+            return (
+              <>
+                <Table.Body key={item}>
+                  <Table.Row>
+                    <Table.Cell>
+                      <Header as="h5" textAlign="left">
+                        {item.name}
+                      </Header>
+                    </Table.Cell>
+                    <Table.Cell singleLine>{item.groupPrefix}</Table.Cell>
+                    <Table.Cell id="centre">
+                      <Image src={item.logo} className="logo" />
+                    </Table.Cell>
+                    <Table.Cell textAlign="right">
+                      <Header as="h5" textAlign="left">
+                        {item.ofstedRating}
+                      </Header>
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </>
+            );
+          })}
+      </Table>
+    </>
   );
 };
 
